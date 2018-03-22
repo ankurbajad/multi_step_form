fb17db4da6f9c2b9b38da2f21e3b9c02dc69c1ad
Ankur Bajad <ankurbajad786@gmail.com>
2018-03-22 14:33:22 +0530

    Added root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => {:registrations => "users/registrations"}
-  root 'users#sign_up'
+  #root 'users/sign_up'
+  devise_scope :user do
+    authenticated :user do
+      root :to => 'devise/registrations#new'
+    end
+    unauthenticated :user do
+      root :to => 'devise/registrations#new', as: :unauthenticated_root
+    end
+  end
 end
